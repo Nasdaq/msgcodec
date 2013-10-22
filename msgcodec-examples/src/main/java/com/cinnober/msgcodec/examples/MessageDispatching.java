a2008360e9d2d9573e433d634f4a3ef8c7d3c89e
Mikael Brannstrom <mikael.brannstrom@cinnober.com>
2013-10-22 16:01:38 +0200

    licence

--- msgcodec-examples/src/main/java/com/cinnober/msgcodec/examples/MessageDispatching.java
+++ msgcodec-examples/src/main/java/com/cinnober/msgcodec/examples/MessageDispatching.java
@@ -1,7 +1,19 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Copyright (c) 2013 Cinnober Financial Technology AB, Stockholm,
+ * Sweden. All rights reserved.
+ *
+ * This software is the confidential and proprietary information of
+ * Cinnober Financial Technology AB, Stockholm, Sweden. You shall not
+ * disclose such Confidential Information and shall use it only in
+ * accordance with the terms of the license agreement you entered into
+ * with Cinnober.
+ *
+ * Cinnober makes no representations or warranties about the suitability
+ * of the software, either expressed or implied, including, but not limited
+ * to, the implied warranties of merchantibility, fitness for a particular
+ * purpose, or non-infringement. Cinnober shall not be liable for any
+ * damages suffered by licensee as a result of using, modifying, or
+ * distributing this software or its derivatives.
  */
 
 package com.cinnober.msgcodec.examples;
